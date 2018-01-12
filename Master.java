00fbfed4fd0b9930432bbc7862e8e08f56f1b1cd
sainathreddypatlolla <patlollasainath@gmail.com>
2018-01-12 14:39:36 +0530

    master

---
+++ Master.java
@@ -0,0 +1,10 @@
+package com.pkrm.GIT.psainath;
+
+public class Master {
+	public static void main(String[] args) {
+		System.out.println("this is master");
+		System.out.println("this is master");
+		System.out.println("this is master");
+	}
+
+}
